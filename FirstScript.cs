4ee26ad7ff78dca0c94eaa5d2dcbc5d62f18fdbc
Avin19 <avinashy1993@gmail.com>
2023-03-31 23:25:16 +0530

    First Commit

---
+++ FirstScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class FirstScript:MonoBehaviour
+{
+    // Variables can be defined here
+    // User define function can be placed here
+    //Start Function that was provide by monobehvaiour
+
+    private void Start()
+    {
+
+    }
+    // Update Function that runs on every frame 
+    private void Update()
+    {
+
+    }
+}
